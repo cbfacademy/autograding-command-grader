60ced6a752c3c0aa204bc9072858ee18080c87c2
Genyus <genyus@gmail.com>
2025-05-06 04:08:56 -0400

    ci: fix undefined variable error

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ function generateResult(status, testName, command, message, duration, maxScore) 
   let score = status === 'pass' ? maxScore : 0
 
   // Look for a pattern like "X of Y ... passed"
-  const match = message.match(/(\d+)\s+of\s+(\d+)\s+.*passed/i)
+  const match = message?.match(/(\d+)\s+of\s+(\d+)\s+.*passed/i)
 
   if (match) {
     const passed = parseInt(match[1], 10)
